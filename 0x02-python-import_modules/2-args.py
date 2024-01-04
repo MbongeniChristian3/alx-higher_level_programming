4a3bdb8cf25a42f2f69c7226463ca6794c8fd19d
MbongeniChristian3 <moyom8660@gmail.com>
2024-01-04 13:12:47 -0800

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} arguments.".format(x))
+    else:
+        print("{} arguments.".format(x))
+
+        for i in range(x0):
+            print("{}: {:s}".format(i + 1))
+
+
