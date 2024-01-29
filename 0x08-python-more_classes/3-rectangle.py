a0543a28f635540203aa1b5252fb839ef5f73fe9
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-01-29 12:22:58 -0800

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -5,7 +5,8 @@
 class Rectangle:
     """Represents a rectangle."""
 
-    def __init__(self.width=0, height=0):
+
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
@@ -41,21 +42,21 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-          def area(self):
+    def area(self):
             """returns the area of the rectangle"""
             return self.__width * self.__height
 
-        def perimeter(self):
+    def perimeter(self):
             """returns the perimeter of the rectangle"""
             if self.__width == 0 or self.__height == 0:
                 return 0
-            return (self__width * 2) + (self.__height * 2)
+            return 2 * (self.__width + self.__height)
 
-        def __str__(self):
+    def __str__(self):
             """returns printable string represantation of the rectagle"""
             string = ""
-            if self>__width != 0  and self.__height != 0:
+            if self.__width != 0  and self.__height != 0:
                 string += "\n".join("#" * self.__width
-                                    for j in range(self.__heght))
+                                    for j in range(self.__height))
             return string
 
